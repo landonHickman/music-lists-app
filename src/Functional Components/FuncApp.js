2c5520c307a4d43f4fcbe67d99f50124dec1ba93
landon hickman <landonhickman12@gmail.com>
2021-06-07 17:31:50 -0600

    func add song works

--- src/Functional Components/FuncApp.js
+++ src/Functional Components/FuncApp.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Header } from 'semantic-ui-react'
+import FuncSongForm from './FuncSongForm'
 import FuncSongs from './FuncSongs'
-let funcSongs = [
+
+const FuncApp = () => {
+  const [funcSongs, setFuncSongs] = useState([
     {id: 1, name: 'Bummerland', artist: 'AJR',},
     {id: 2, name: 'Float On', artist: 'Modest Mouse',},
     {id: 3, name: 'The Getaway', artist: 'Red Hot Chilli Peppers',},
     {id: 4, name: 'Dani California', artist: 'Red Hot Chilli Peppers',},
     {id: 5, name: 'Solitary Man', artist: 'Johnny Cash',},
-]
+  ])
 
-const FuncApp = () => {
+  const addSong = (song) => {
+    console.log('song',song)
+    setFuncSongs( [song, ...funcSongs])
+    console.log(funcSongs)
+  }
+
   return(
     <Container style={{paddingTop: '25px'}}>
       <Header as='h1'>React Functional Music App</Header>
+      <FuncSongForm addSong={addSong}/>
       <FuncSongs funcSongs={funcSongs}/>
     </Container>
   )
--- src/Functional Components/FuncSongForm.js
+++ src/Functional Components/FuncSongForm.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useState,} from 'react'
+import { Form } from 'semantic-ui-react'
 
-const FuncSongForm = () => {
+const FuncSongForm = (props) => {
+  const {id, addSong} = props
+  const [name, setName] = useState(props.name ? props.name : '')
+  const [artist, setArtist] = useState(props.artist ? props.artist : '')
+
+  const rando = () => {
+    return Math.random()
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(id){
+     
+      
+    } else {
+
+      console.log('look here', {name, artist, rando})
+     addSong({name: name, artist: artist, id: rando()})
+    }
+  }
+  
+
   return(
-    <div>
-      <h1>FuncSongForm</h1>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group widths="equal">
+        <Form.Input
+        fluid
+        label="Name"
+        placeholder="Name"
+        name="name"
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+        style={{width: '200px'}}
+        />
+        <Form.Input
+        fluid
+        label="Artist"
+        placeholder="Artist"
+        name="artist"
+        value={artist}
+        onChange={(e)=>setArtist(e.target.value)}
+        style={{width: '200px'}}
+        />
+        <Form.Button color='blue'>{id ? 'Edit' : 'Add'}</Form.Button>
+      </Form.Group>
+    </Form>
   )
 }
+  
+
 export default FuncSongForm
